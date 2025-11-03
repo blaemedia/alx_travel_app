172c744d3c2c3dca43f1d8e7666231b26fe460c8
Emmanuel <blaemedia26@gmail.com>
2025-11-03 07:29:38 +0100

    restoring the listing folder path to the initial

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'alx_travel_app.listings',
+    'listings',
+
     'corsheaders', 
     'rest_framework',
     'drf_yasg',
--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listing_list, name='listing-list'),   
+    path('', views.listing_list, name='listing_list'),
 ]
